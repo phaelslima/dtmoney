babb9837edb61a8d40551d8ae8e6992777cd70ac
Raphael Lima <phaelslima@gmail.com>
2022-03-18 00:04:52 -0300

    feat: add useTransactions hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
+import { TransactionsProvider } from "./hooks/useTransactions";
+
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root')
@@ -21,11 +23,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransitionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
